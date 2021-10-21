e4349a9f35f40abdfd1212c4d494a3e153b7301a
Katie Pundt <kzpundt@gmail.com>
2021-10-21 13:36:52 -0700

    add validation
    Co-Authored-by: Mike Watkins <michaelhugheswatkins@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -6,42 +6,49 @@ $(document).ready(function() {
     const animal = $("input:radio[name=animal]:checked").val();
     const quality = $("input:radio[name=quality]:checked").val();
 
-    if (name === '') {
+    if (name === "") {
       $('#error').show();
-    } else {
-      $('#error').hide();
+      $('#gryffindor').hide();
+      $('#slytherin').hide();
+      $('#ravenclaw').hide();
+      $('#hufflepuff').hide();
+      $('#anyHouse').hide();
     }
-
-    if (color === "red" && animal === "owl" && quality === "loyal") {
+      else if (color === "red" && animal === "owl" && quality === "loyal") {
       $('#gryffindor').show();
       $('#slytherin').hide();
       $('#ravenclaw').hide();
       $('#hufflepuff').hide();
       $('#anyHouse').hide();
+      $('#error').hide();
     } else if (color === "green" && animal === "cat" &&  quality === "cunning") {
       $('#slytherin').show();
       $('#gryffindor').hide();
       $('#ravenclaw').hide();
       $('#hufflepuff').hide();
       $('#anyHouse').hide();
+      $('#error').hide();
     } else if (color === "blue" && animal === "mouse" && quality === "intelligent") {
       $('#ravenclaw').show();
       $('#gryffindor').hide();
       $('#slytherin').hide();
       $('#hufflepuff').hide();
       $('#anyHouse').hide();
+      $('#error').hide();
     } else if (color === "yellow" && animal === "toad" && quality === "loyal") {
       $('#hufflepuff').show();
       $('#gryffindor').hide();
       $('#slytherin').hide();
       $('#ravenclaw').hide();
       $('#anyHouse').hide();
+      $('#error').hide();
     } else {
       $('#gryffindor').hide();
       $('#slytherin').hide();
       $('#ravenclaw').hide();
       $('#hufflepuff').hide();
       $('#anyHouse').show();
+      $('#error').hide();
     }
 
     $(".name").text(name);
